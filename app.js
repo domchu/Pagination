94d9d897e6f57e518600438b2f929d472408faba
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-04-21 13:55:51 +0100

    commenting

--- app.js
+++ app.js
@@ -3,29 +3,27 @@ const next = document.querySelector(".next");
 let pagination = document.querySelectorAll(".page");
 
 let current = 0;
-
 pagination[current].classList.add("current");
 
+// the next button part
 next.addEventListener("click", () => {
   current = current + 1;
   if (current > pagination.length - 1) {
     current = 0;
   }
-  // console.log(current);
   for (let index = 0; index < pagination.length; index++) {
     pagination[index].classList.remove("current");
   }
   pagination[current].classList.add("current");
 });
-
-// prev.addEventListener("click", () => {
-//   current = current - 1;
-//   // console.log(current);
-//   if (current < pagination.length - 1) {
-//     current = current + 1;
-//   }
-//   for (let index = 0; index < pagination.length; index++) {
-//     pagination[index].classList.remove("current");
-//   }
-//   pagination[current].classList.add("current");
-// });
+// the prev part of the project
+prev.addEventListener("click", () => {
+  current = current - 1;
+  if (current < 0) {
+    current = pagination.length - 1;
+  }
+  for (let index = 0; index < pagination.length; index++) {
+    pagination[index].classList.remove("current");
+  }
+  pagination[current].classList.add("current");
+});
