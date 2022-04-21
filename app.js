b00ee558d3aabd5c7bd353eb76181fa98cf1050c
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-04-21 12:15:34 +0100

    toggling the next button

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
-let pagination = document.querySelectorAll(".pagination");
-let pageIndex = 0;
+let pagination = document.querySelectorAll(".page");
 
-prev.addEventListener("click", () => {
-  pageIndex = pageIndex + 1;
-  if (pageIndex < pagination.length) {
-    pageIndex = 1;
+let current = 0;
+
+pagination[current].classList.add("current");
+
+next.addEventListener("click", () => {
+  current = current + 1;
+  if (current > pagination.length - 1) {
+    current = 0;
   }
+  // console.log(current);
   for (let index = 0; index < pagination.length; index++) {
-    pagination[index].style.display = "none";
-    pagination[pageIndex - 1].style.display = "block";
+    pagination[index].classList.remove("current");
   }
+  pagination[current].classList.add("current");
 });
+
+// prev.addEventListener("click", () => {
+//   current = current - 1;
+//   // console.log(current);
+//   if (current < pagination.length - 1) {
+//     current = current + 1;
+//   }
+//   for (let index = 0; index < pagination.length; index++) {
+//     pagination[index].classList.remove("current");
+//   }
+//   pagination[current].classList.add("current");
+// });
